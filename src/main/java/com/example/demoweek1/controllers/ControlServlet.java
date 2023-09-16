fdc610a8ea5fb2e54f41ccd7e80d00004dae8b74
ToHieuDong <hieudong.dongthanh.02@gmail.com>
2023-09-17 00:08:00 +0700

    Xử lý cơ bản

--- src/main/java/com/example/demoweek1/controllers/ControlServlet.java
+++ src/main/java/com/example/demoweek1/controllers/ControlServlet.java
@@ -12,15 +12,28 @@ import java.io.PrintWriter;
 @WebServlet(name = "controlServlet", value = "/ctrl")
 public class ControlServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<form action=\"\" method=\"get\">\n" +
+        out.println("<form action=\"summit\" method=\"get\">\n" +
                 "    <input type=\"text\" name=\"user\" id=\"user\" placeholder=\"Tài khoản\"> <br>\n" +
                 "    <input type=\"password\" name=\"password\" id=\"password\" placeholder=\"Mật khẩu\"> <br>\n" +
                 "    <button type=\"submit\">Đăng nhập</button>\n" +
                 "</form>");
         out.println("</body></html>");
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action==null || action.isEmpty())
+            response.reset();
+        else if ("summit".equals(action)) {
+            String user = request.getParameter("user");
+            String password = request.getParameter("password");
+            response.sendRedirect("http://localhost:8080/LTWWW_WEEK1/login.html");
+        } else
+            response.reset();
     }
 }
